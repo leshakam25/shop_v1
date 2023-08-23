ca5cc19ba46129118ae396ad65a7c46adc026ecc
leshakam25 <leshakam25@gmail.com>
2023-08-23 17:42:31 +0300

    commit

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,14 +1,21 @@
 import {Product} from "@/interfaces/product.interface";
-import React from "react";
 
-export const getProductById = async (_id: string): Promise<Product | undefined>  => {
+export const getProductById = async (_id: string): Promise<Product | undefined> => {
     const url = `http://localhost:4000/product/${_id}`
     try {
-        const res = await fetch(url, {
-            cache: 'no-store'
-        })
+        const res = await fetch(url, {cache: 'no-store'});
         return res.json()
     } catch (error) {
         console.log(error)
     }
 }
+
+export const getProducts = async (): Promise<Product[] | undefined> => {
+    const url = 'http://localhost:4000/product/';
+    try {
+        const res = await fetch(url, {cache: "no-store"});
+        return await res.json();
+    } catch (error) {
+        console.log("Error loading products: ", error);
+    }
+};
